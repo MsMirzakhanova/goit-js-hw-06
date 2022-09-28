ed48740b3fd53747a6a23f8a52f5e390651c7f30
MsMirzakhanova <mirzakhanovamari@gmail.com>
2022-09-28 13:22:20 +0300

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingrList = document.querySelector("#ingredients");
+const elements = ingredients.map((ingredient) => {
+  const ingrEl = document.createElement(`li`);
+  ingrEl.textContent = ingredient;
+  ingrEl.classList.add(`item`);
+  return ingrEl;
+});
+
+ingrList.append(...elements);
+console.log(ingrList);
