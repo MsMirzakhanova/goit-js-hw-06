dca020e32829da31e5398a06a0ae587d58adb6b0
MsMirzakhanova <mirzakhanovamari@gmail.com>
2022-10-03 14:32:29 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector(`.change-color`);
+const colorRef = document.querySelector(`.color`);
+const body = document.querySelector(`body`);
+
+//console.log(btnChangeColor);
+
+btnChangeColor.addEventListener(`click`, (event) => {
+  const randomColor = getRandomHexColor();
+  body.style.background = randomColor;
+  colorRef.textContent = randomColor;
+});
