fbf29fcf095eccfdbfc016065c550e891a06839d
MsMirzakhanova <mirzakhanovamari@gmail.com>
2022-10-03 19:30:16 +0300

    task 10 process

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,8 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const input = document.querySelector(`#controls > input`);
+//console.log(input);
+
+const amount = input.target.value;
+console.log(amount);
