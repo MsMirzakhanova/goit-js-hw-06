1b8156633415098e4839a4b101b9b89e8e5a03d6
MsMirzakhanova <mirzakhanovamari@gmail.com>
2022-10-03 14:12:24 +0300

    task 7

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,5 @@ const controlEl = document.querySelector("#font-size-control");
 const spanTextRef = document.querySelector(`#text`);
 
 controlEl.addEventListener("click", (event) => {
-  const text = spanTextRef.textContent;
-  text.style.fontSize = controlEl.value;
+  spanTextRef.style.fontSize = controlEl.value + "px";
 });
