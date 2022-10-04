ceeece01f02bcd468b62946c46fb62260fc717dc
MsMirzakhanova <mirzakhanovamari@gmail.com>
2022-10-04 17:32:56 +0300

    task 5 correct

--- js/task-05.js
+++ js/task-05.js
@@ -7,10 +7,18 @@
 const refs = {
   input: document.querySelector(`#name-input`),
   output: document.querySelector(`#name-output`),
+  span: document.querySelector(`span`),
 };
 
 refs.input.addEventListener(`input`, onInputChange);
 
 function onInputChange(event) {
   refs.output.textContent = event.currentTarget.value;
+}
+
+refs.input.addEventListener(`change`, onOutputChange);
+
+function onOutputChange(event) {
+  refs.output.disabled = !event.target.checked;
+  refs.output.textContent = span.textContent;
 }
